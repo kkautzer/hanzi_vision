6b110288fd5fc52877d9ae7c23a0959f8d68f5b1
Keagan Kautzer <kkautzer05@gmail.com>
2025-08-28 14:22:35 -0500

    Add CORS, JSON Responses, Update Eval Model Name

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS as cors
 import cv2
 import numpy as np
 from character_classifier.eval_single import evaluate
 
 app = Flask(__name__)
+cors(app)
 
 @app.route('/') # basic route to test the connection to the API
 def hello_world():
@@ -11,27 +13,27 @@ def hello_world():
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate_image():
-        
+    print(request)
     if 'image' not in request.files:
-        return "No image provided", 400
+        return jsonify({"message": "No image provided"}), 400
     
-    image = request.files['image'] # must match the value of the HTML `name` attribute for the image
+    image = request.files.get('image') # must match the value of the HTML `name` attribute for the image
 
     if image.filename == '':
-        return "No selected file", 400
+        return jsonify({"message": "No selected file"}), 400
     
     if image:
         try:
             image_array = np.frombuffer(image.read(), np.uint8) # read image bytes, convert to np.uint8 array
             image = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED) # convert np.uint8 array to a cv2 image            
             # dictionary of form {id: [id], label: [char]}
-            return dict(zip( ("id", "label"), evaluate(image, "model-GoogLeNet-500-aug-0.2") ))
+            return jsonify(dict(zip( ("id", "label"), evaluate(image, "model-GoogLeNet-500-1.0") ) )) , 200
 
         except Exception as e:
             print(e)
-            return "Failed to evaluate image"
+            return jsonify({"message": "Failed to evaluate image"}), 500
     else:
-        return "No image provided [2]"
+        return jsonify({"message": "No image provided [2]"}), 400
     
     
 if __name__ == "__main__":

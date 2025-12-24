406390ef8df158c4df314cb1b70f3fd0de63a621
Keagan Kautzer <kkautzer05@gmail.com>
2025-12-23 18:11:13 -0600

    Revert "implement model vs. highest accuracy table generator"
    This reverts commit c944fa4e645910462715002c0b42c2fbbcba460e.

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,78 +1,36 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+
 import os
 import json
 
 save_path_root = None
 
 ## num. characters vs. highest accuracy analysis
-def nCharsVsAccuracyTable():
-    if save_path_root:
-        save_path = f'{save_path_root}/chars_accuracy_table.png'
-    else:
-        save_path = None
-        
+def nCharsVsAccuracy():
     # read metadata files from `./models/exports/metadata` to get highest accuracy for each model
-    filenames = os.listdir('./model/exports/metadata')
+    models = os.listdir('./model/exports/metadata')
     
-    # create mapping between nchars and highest accuracy
-    char_accuracy_data = pd.DataFrame(
-        columns=['architecture', 'model_name', 'nchars', 'max_val_accuracy', 'max_val_epoch', 'epochs']
-    )
-    column_translate = {
-        'architecture': "Underlying Architecture",
-        "model_name": "Model Name",
-        "nchars": "Number of Characters",
-        "max_val_accuracy": "Highest Accuracy",
-        "max_val_epoch": "Highest Accuracy Epoch",
-        "epochs": "Epochs"
-    }
+    # create mapping between model name and highest accuracy
+    model_accuracies = {}
+    for model in models:
+        with open(f'./model/exports/metadata/{model}', 'r') as f:
+            metadata = json.load(f)
+            model_accuracies[metadata['nchars']] = metadata['highest_accuracy']
     
-    # read metadata & build dataframe
-    for fn in filenames:
-        with open(f'./model/exports/metadata/{fn}', 'r', encoding='utf-8') as f:
-            char_accuracy_data.loc[len(char_accuracy_data)] = json.load(f)
-
-    # update column names
-    char_accuracy_data.rename(columns=column_translate, inplace=True)    
-        
-    # create table
-    fig, ax = plt.subplots(figsize=(8, 2.5))
-    ax.axis('off')
-    table = plt.table(
-        cellText=char_accuracy_data.values,
-        colLabels=char_accuracy_data.columns,
-        loc='center',
-        cellLoc='center'
-    )    
+    # create graph of num. characters vs. highest accuracy
     
-    table.auto_set_font_size(False)
-    table.set_fontsize(7)
-    table.scale(1, 1.4)
-
-    plt.tight_layout()
+    # show (+ save) plot
     
-    if save_path is not None:
-        os.makedirs(save_path_root, exist_ok=True)
-        plt.savefig(save_path, dpi=300)
-        print(f'Saved figure to {save_path}!')
-    
-    plt.show()
-        
+    pass
 
 ## epochs vs. accuracy analysis for each character set
-def epochsVsAccuracyCurve():
-    print("--- NOT YET IMPLEMENTED ---")
-    return
-
+def epochsVsAccuracy():
     # read training_data.csv to get accuracy vs. epoch data for each model
     training_data = pd.read_csv('./model/exports/training_data.csv')
     
-    # divide dataframe by model name, then sort increasing by epoch
+    # sort by model name
     
-    # for each [exported] model, create a graph of accuracy vs. epoch
-
-    # create an aggregate graph of accuracy vs. epoch (one line per model)
+    # create graph of accuracy vs. epoch for each character set
     
     # show (+ save) plot
     
@@ -80,9 +38,9 @@ def epochsVsAccuracyCurve():
 
 ## main method - include a switch to save plots or just show them
 if __name__ == "__main__":
-    save_plots = True
+    save_plots = False
     if save_plots:
-        save_path_root = './analysis/figures'
+        save_path_root = './analysis/plots/'
 
-    nCharsVsAccuracyTable()
-    # epochsVsAccuracyCurve()
+    nCharsVsAccuracy()
+    epochsVsAccuracy()
